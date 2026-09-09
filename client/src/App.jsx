a0d48d855f4325d3eb04e3ab4f217f0bae1a0e25
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-09-09 09:12:27 +0500

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/' element={<SignIn/>}></Route>
+      <Route path='/' element={<SignUp/>}></Route>
+      <Route path='/' element={<About/>}></Route>
+      <Route path='/' element={<Profile/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
