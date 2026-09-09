8283a7f46bf7c6c77240063d811e1dc0eb47475e
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-09-09 22:23:20 +0500

    Header component build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/' element={<SignIn/>}></Route>
-      <Route path='/' element={<SignUp/>}></Route>
-      <Route path='/' element={<About/>}></Route>
-      <Route path='/' element={<Profile/>}></Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        {/* // to show the header on all the pages we have to import it here */}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<SignIn />}></Route>
+          <Route path="/" element={<SignUp />}></Route>
+          <Route path="/" element={<About />}></Route>
+          <Route path="/" element={<Profile />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
