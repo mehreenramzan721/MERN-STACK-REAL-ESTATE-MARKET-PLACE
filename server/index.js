abf12065060bbea3fe5c2dbf7582be25401e2d76
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-09-10 20:56:30 +0500

    create a test api route

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+// importing the routes
+import userRoute from "./routes/userRoute.js";
 const app = express();
 dotenv.config();
 
+// mongoose connection
 mongoose
   .connect(process.env.MONGO_DB_URI)
   .then(() => {
@@ -13,6 +16,9 @@ mongoose
     console.log(`Caught an error : ${err}`);
   });
 
+app.use("/api/user", userRoute);
+
+//loading the server
 app.listen(2000, () => {
   console.log(`PORT is running at port 2000`);
 });
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -20,8 +20,9 @@ const userSchema = new mongoose.Schema(
   // it records the time of creation and updation of data
   { timestamps: true },
 );
-// we name our folder that we are going to make inside mongo db for user with the name User
-// because we are creating only 1 user and when later on we added more users then mongo db will set it to Users by default
+// we name our folder that we are going to make inside mongo db for user with the name users
+//Mongoose automatically pluralizes and lowercases the model name to decide the collection name. So "User" → collection users in MongoDB, even before you have more than one user.
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
