39eb9e31f643c6d4818325b93f36616d6bc26a03
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-09-10 13:29:06 +0500

    db debugging

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 const app = express();
 dotenv.config();
-console.log("URI loaded:", process.env.MONGO_DB_URI);
 
 mongoose
   .connect(process.env.MONGO_DB_URI)
@@ -13,8 +12,7 @@ mongoose
   .catch((err) => {
     console.log(`Caught an error : ${err}`);
   });
-const u = process.env.MONGO_DB_URI;
-console.log("length:", u?.length);
+
 app.listen(2000, () => {
   console.log(`PORT is running at port 2000`);
 });
