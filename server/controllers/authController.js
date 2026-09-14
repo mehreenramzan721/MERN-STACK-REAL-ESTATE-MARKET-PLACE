0021f28db08eff7caca1c5634966cfd8848c7c7c
Mehreen Ramzan <mehreenramzan721@gmail.com>
2026-09-14 22:33:13 +0500

    create sign in route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,8 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import { errorhandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   // req.body is the information we get from the browser
   // by using destructuring we are actually saving the info that we have in our req.body
@@ -12,6 +15,26 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: `User created successfully! ` });
   } catch (error) {
     // res.status(500).json(error.message);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorhandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorhandler(401, "Wrong cridentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
     next(error);
   }
 };
